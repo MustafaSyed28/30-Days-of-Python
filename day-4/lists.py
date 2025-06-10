4b4598785068f2cb9750a9fa19a63f59713ae1c3
MustafaSyed28 <syed.mustafa0328@gmail.com>
2025-06-09 22:14:42 -0500

    day-4

---
+++ day-4/lists.py
@@ -0,0 +1,23 @@
+# lists
+
+list1 = ["apple", "ball", "cat", "dog"]
+print(list1)
+print(list1[0])
+print(len(list1))
+
+list2 = list((1, 2, 3, 4))
+print(list2)
+print(list2[0:2])
+print(type(list2))
+
+list1.append("egg")
+list2.insert(0, 0)
+print(list1)
+print(list2)
+
+list2.pop(0)
+print(list2)
+
+
+
+
