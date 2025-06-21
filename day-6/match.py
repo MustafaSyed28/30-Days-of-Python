619d31345a3f2ac102dfb57a6c22a88626230ad4
MustafaSyed28 <syed.mustafa0328@gmail.com>
2025-06-20 22:46:57 -0500

    day-7

--- day-6/match.py
+++ day-6/match.py
@@ -16,3 +16,21 @@ match day:
     print("Saturday")
   case 7:
     print("Sunday")
+
+
+person = ("Alice", 25)
+
+match person:
+    case (name, 25):
+        print(f"{name} is 25 years old.")
+    case _:
+        print("No match.")    
+
+
+colors = ["red", "blue", "green"]
+
+match colors:
+    case [a, b, c]:
+        print(f"Three colors: {a}, {b}, {c}")
+    case _:
+        print("Color list doesn't have 3 items")        
